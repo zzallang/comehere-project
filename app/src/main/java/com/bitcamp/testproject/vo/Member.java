50f0e70e7c24436fd3db7588234e67d599ed7679
trev0129 <pbm9393@gmail.com>
2022-11-07 17:32:21 +0900

    병민1107 17:30

--- app/src/main/java/com/bitcamp/testproject/vo/Member.java
+++ app/src/main/java/com/bitcamp/testproject/vo/Member.java
@@ -25,12 +25,57 @@ public class Member {
   // 모임참여 명단에서 주최자 여부
   private boolean auth;
 
+  // 모임 참여 명단에서 나이대
+  private String ageGroup;
+
   @Override
   public String toString() {
     return "Member [no=" + no + ", name=" + name + ", id=" + id + ", email=" + email + ", password="
         + password + ", password1=" + password1 + ", nickname=" + nickname + ", tel=" + tel
         + ", birthday=" + birthday + ", sex=" + sex + ", filepath=" + filepath + ", createdDate="
-        + createdDate + ", active=" + active + ", black=" + black + ", auth=" + auth + "]";
+        + createdDate + ", active=" + active + ", black=" + black + ", favoriteRegion="
+        + favoriteRegion + ", favoriteSports=" + favoriteSports + ", auth=" + auth + ", ageGroup="
+        + ageGroup + "]";
+  }
+
+  public int getNo() {
+    return no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
   }
 
   public String getPassword1() {
@@ -41,165 +86,101 @@ public class Member {
     this.password1 = password1;
   }
 
-  public int getNo() {
-    return no;
-  }
-
-
-  public void setNo(int no) {
-    this.no = no;
-  }
-
-
-  public String getName() {
-    return name;
-  }
-
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-
-  public String getId() {
-    return id;
-  }
-
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-
-  public String getEmail() {
-    return email;
-  }
-
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-
-  public String getPassword() {
-    return password;
-  }
-
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-
   public String getNickname() {
     return nickname;
   }
-
 
   public void setNickname(String nickname) {
     this.nickname = nickname;
   }
 
-
   public String getTel() {
     return tel;
   }
-
 
   public void setTel(String tel) {
     this.tel = tel;
   }
 
-
   public Date getBirthday() {
     return birthday;
   }
-
 
   public void setBirthday(Date birthday) {
     this.birthday = birthday;
   }
 
-
   public boolean isSex() {
     return sex;
   }
-
 
   public void setSex(boolean sex) {
     this.sex = sex;
   }
 
-
   public String getFilepath() {
     return filepath;
   }
-
 
   public void setFilepath(String filepath) {
     this.filepath = filepath;
   }
 
-
   public Date getCreatedDate() {
     return createdDate;
   }
-
 
   public void setCreatedDate(Date createdDate) {
     this.createdDate = createdDate;
   }
 
-
   public boolean isActive() {
     return active;
   }
-
 
   public void setActive(boolean active) {
     this.active = active;
   }
 
-
   public boolean isBlack() {
     return black;
   }
-
 
   public void setBlack(boolean black) {
     this.black = black;
   }
 
-
-  public boolean isAuth() {
-    return auth;
-  }
-
-
-  public void setAuth(boolean auth) {
-    this.auth = auth;
-  }
-
-
   public List<FavoriteRegion> getFavoriteRegion() {
     return favoriteRegion;
   }
-
 
   public void setFavoriteRegion(List<FavoriteRegion> favoriteRegion) {
     this.favoriteRegion = favoriteRegion;
   }
 
-
   public List<FavoriteSports> getFavoriteSports() {
     return favoriteSports;
   }
-
 
   public void setFavoriteSports(List<FavoriteSports> favoriteSports) {
     this.favoriteSports = favoriteSports;
   }
 
+  public boolean isAuth() {
+    return auth;
+  }
+
+  public void setAuth(boolean auth) {
+    this.auth = auth;
+  }
+
+  public String getAgeGroup() {
+    return ageGroup;
+  }
+
+  public void setAgeGroup(String ageGroup) {
+    this.ageGroup = ageGroup;
+  }
 
 
 
