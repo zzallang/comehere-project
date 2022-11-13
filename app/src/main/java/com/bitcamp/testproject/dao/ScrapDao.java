5d7f7d7fa90c0180952957f8a2df3ed71552d56b
youjedong <mraiou1@naver.com>
2022-11-13 13:51:45 +0900

    제동 - 스크랩 기능 추가
    - detail.html
    1. 스크랩 버튼 작성자와 비회원은 보이게않게 구현 (완)
    2. 스크랩 등록, 삭제 ajax로 구현 (완)
    
    - 우선 sweetalert npm으로 다운
    
    - 스크랩 소스(Dao, Controller, Service, vo) 추가
    
    -------------------------------------------------------------------
    - updateForm html
    1. 헤더 푸터 추가 (미완)
    
    - 상세조회
    *눌렀을 때 비회원은 로그인 유도 해야함 (미완)
    3. 댓글 페이징 active 복잡해서 우선 보류 (미완)
    
    - 챌린지 게시판 수정
    1. 크기가 너무 큼.. 수정할 수 있을지 모르겠지만 우선 체크(미완)
    
    - 커뮤니티 메인 추가
    1. 주간 인기글 할 수 있을지 고민
    
    - 페이징 추가
    1. 게시글 수정 삭제 시 현재 페이지로 reflesh 추가 (미완)

---
+++ app/src/main/java/com/bitcamp/testproject/dao/ScrapDao.java
@@ -0,0 +1,13 @@
+package com.bitcamp.testproject.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import com.bitcamp.testproject.vo.Scrap;
+
+@Mapper
+public interface ScrapDao {
+
+  int insert(Scrap scrap);
+
+  int delete(Scrap scrap);
+
+}
