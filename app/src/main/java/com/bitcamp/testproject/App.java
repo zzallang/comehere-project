a98c7be582d0ee90a690a2971ca6bd78164c1bc4
sik <hunsik0206@gmail.com>
2022-12-31 01:06:12 +0900

    feat: README

--- app/src/main/java/com/bitcamp/testproject/App.java
+++ app/src/main/java/com/bitcamp/testproject/App.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("비트캠프 프로젝트");
     SpringApplication.run(App.class, args);
   }
 
